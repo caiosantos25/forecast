a6b496d7ff1f75e18e761ea132f9f1c1e1e5ae52
caiosantos25 <157501871+caiosantos25@users.noreply.github.com>
2024-01-23 12:00:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def main():
     import time
     import numpy as np
     import pandas as pd
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
+    import spacy
     import warnings
     st.title("Olá Cadastrer")
     st.write("texto 1")
