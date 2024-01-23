3457ca9ba3ffd0f47ca02de1dc0fdd62239d9c45
caiosantos25 <157501871+caiosantos25@users.noreply.github.com>
2024-01-23 11:53:37 -0300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 def main():
-    import matplotlib
-    matplotlib.use("agg")
     import matplotlib.pyplot as plt
     import streamlit as st
     import pandas as pd
